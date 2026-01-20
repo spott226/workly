773010723cbae0b44340f8c8877eb13e50b733c8
Leniel Montoya <spott226@gmail.com>
2026-01-19 23:46:04 -0600

    fix: listen on 0.0.0.0 for railway

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
-const { databaseUrl } = require('./env');
 
 const pool = new Pool({
-  connectionString: databaseUrl,
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;

7014784a717fd8f6ac88ec7527b6fa36bb4efd89
Leniel Montoya <spott226@gmail.com>
2026-01-19 23:49:20 -0600

    fix: listen on railway

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`SaaS Citas running on port ${PORT}`);
 });
+
 

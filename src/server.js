c7876efac9fb5c9d9d18bf753ad65692909a7704
Leniel Montoya <spott226@gmail.com>
2026-01-19 23:53:21 -0600

    port 0.0.0.

--- src/server.js
+++ src/server.js
@@ -4,8 +4,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`SaaS Citas running on port ${PORT}`);
 });
-
 
